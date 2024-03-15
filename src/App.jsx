0a6b510c217b910773e2ff7280ddabb11fcbeacb
Nicolai Klokmose <angryniczorn@gmail.com>
2024-03-15 10:55:03 +0100

    first push with tailwind.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
 import "./App.css";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <span className="inline-grid grid-cols-3 gap-4 hover:bg-red-700">
+        <span className="border-solid border-2 border-black rounded p-5 hover:bg-red-700">01</span>
+        <span className="border-solid border-2 border-black rounded p-5">02</span>
+        <span className="border-solid border-2 border-black rounded p-5">03</span>
+        <span className="border-solid border-2 border-black rounded p-5">04</span>
+        <span className="border-solid border-2 border-black rounded p-5">05</span>
+        <span className="border-solid border-2 border-black rounded p-5">06</span>
+      </span>
+      <span className="inline-grid grid-cols-3 gap-4 ml-10">
+        <span className="border-solid border-2 border-black rounded p-5">01</span>
+        <span className="border-solid border-2 border-black rounded p-5">02</span>
+        <span className="border-solid border-2 border-black rounded p-5">03</span>
+        <span className="border-solid border-2 border-black rounded p-5">04</span>
+        <span className="border-solid border-2 border-black rounded p-5">05</span>
+        <span className="border-solid border-2 border-black rounded p-5">06</span>
+      </span>
+    </>
+  );
 }
-
 export default App;
