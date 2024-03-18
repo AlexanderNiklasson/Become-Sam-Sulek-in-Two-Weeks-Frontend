db582bd76a47525821c388b6a74d41fbd41df85b
Nicolai Klokmose <angryniczorn@gmail.com>
2024-03-18 17:19:31 +0100

    added logo and some animation stuff

--- src/components/workout_list/workout_item/index.jsx
+++ src/components/workout_list/workout_item/index.jsx
@@ -1,6 +1,6 @@
 export function WorkoutItem({ workout }) {
   return (
-    <div className="bg-white rounded-lg overflow-hidden shadow-md mb-4 hover:text-customLightblue hover:bg-customPurple flex items-center">
+    <div className="bg-white rounded-lg overflow-hidden shadow-md mb-4 hover:text-customPurple hover:border border-customPurple flex items-center">
       <div className="p-4 flex-1">
         <h2 className="text-xl font-semibold mb-2">{workout.name}</h2>
         <p className="text-gray-700 mb-2">
@@ -15,16 +15,24 @@ export function WorkoutItem({ workout }) {
         <p className="text-gray-700 mb-2">
           <strong>Primary Muscles:</strong> {workout.primaryMuscles.join(", ")}
         </p>
-        <a
-          href={`/workout/${workout.id}`}
-          className="text-purple-500 hover:underline">
-          View Details
-        </a>
+        <div className="flex items-center">
+          <a
+            href={`/workout/${workout.id}`}
+            className="text-purple-500 hover:underline"
+          >
+            View Details
+          </a>
+          <img
+            src="../src/assets/view-details.png"
+            alt="logo"
+            className="h-12 w-12 text-customPurple mr-5 ml-5 animate-pulse"
+          />
+        </div>
       </div>
       <img
         src={`/${workout.images[0]}`}
         alt={workout.name}
-        className="w-32 h-32 object-cover"
+        className="w-32 h-32 object-cover mr-5"
       />
     </div>
   );
