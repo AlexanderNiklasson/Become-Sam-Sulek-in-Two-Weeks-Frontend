c41243a21a84f041531ab82bd5b000178c901383
Nicolai Klokmose <angryniczorn@gmail.com>
2024-03-18 15:16:31 +0100

    made workoutlist scrollable and depend on navbar and userpref widths

--- src/components/workout_list/index.jsx
+++ src/components/workout_list/index.jsx
@@ -1,19 +1,25 @@
 import WorkoutItem from "./workout_item/index";
 
-export default function WorkoutList(props) {
-    const { workouts, setDataFetched } = props;
-    console.log("Test logging of top of workoutlist component")
-
-    return (
-    <main className="max-w-md mx-auto bg-white shadow-md rounded-lg overflow-hidden">
-      <div className="p-4">
-        {workouts && workouts.length > 0 ? (
-          workouts.map((workout) => (
-            <WorkoutItem key={workout.id} workout={workout} />
-          ))
-        ) : (
-          <p className="text-gray-700">No workouts available</p>
-        )}
+export default function WorkoutList({ workouts }) {
+  return (
+    <main className="flex-1 flex">
+      <div className="w-64 h-screen overflow-y-auto border-r border-gray-300">
+        {/* Left Navbar */}
+      </div>
+      <div className="flex-1 overflow-y-auto">
+        {/* Workout List */}
+        <div className="p-5 h-screen overflow-y-auto">
+          {workouts && workouts.length > 0 ? (
+            workouts.map((workout) => (
+              <WorkoutItem key={workout.id} workout={workout} />
+            ))
+          ) : (
+            <p className="text-gray-700">No workouts available</p>
+          )}
+        </div>
+      </div>
+      <div className="w-64 h-screen overflow-y-auto border-l border-gray-300">
+        {/* Right User Preferences */}
       </div>
     </main>
   );
